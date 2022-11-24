dbc54eed81bbf5f5e2bb7761d564ab0846063cc1
Руслан <@DESKTOP-1KS25B2>
2022-11-24 12:16:51 +0300

    Добавьте файлы проекта.

---
+++ spa/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spa
+{
+    static class User
+    {
+        public static string login { get; set; }
+        public static string password;
+    }
+}
